cd2c655823e5318a24a9362564c18c4e34a1972f
franss <franss40@gmail.com>
2024-06-17 11:25:43 +0200

    react query

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,65 @@
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query"
+import { getNotes, createNote, updateNote } from './request'
+
 const App = () => {
+  const queryClient = useQueryClient()
+
+  const newNoteMutation = useMutation({
+    mutationFn: createNote,
+    onSuccess: (newNote) => {
+      //queryClient.invalidateQueries({ queryKey: ["notes"] })
+      const notes = queryClient.getQueryData(['notes'])
+      queryClient.setQueryData(['notes'], notes.concat(newNote))
+    },
+  })
+
+  const updateNoteMutation = useMutation({
+    mutationFn: updateNote,
+    onSuccess: (updateNote) => {
+      //queryClient.invalidateQueries({ queryKey: ["notes"] })
+      const notes = queryClient.getQueryData(['notes'])
+      queryClient.setQueryData(["notes"], notes.map(note => note.id === updateNote.id ? updateNote : note))
+    },
+  })
+
   const addNote = async (event) => {
     event.preventDefault()
     const content = event.target.note.value
-    event.target.note.value = ''
-    console.log(content)
+    event.target.note.value = ""
+
+    newNoteMutation.mutate({ content, important: true })
   }
 
   const toggleImportance = (note) => {
-    console.log('toggle importance of', note.id)
+    updateNoteMutation.mutate({ ...note, important: !note.important })
   }
 
-  const notes = []
+  const result = useQuery({
+    queryKey: ["notes"],
+    queryFn: getNotes,
+    refetchOnWindowFocus: false
+  })
 
-  return(
+  if (result.isLoading) {
+    return <div>loading data...</div>
+  }
+
+  const notes = result.data
+
+  return (
     <div>
       <h2>Notes app</h2>
       <form onSubmit={addNote}>
         <input name="note" />
         <button type="submit">add</button>
       </form>
-      {notes.map(note =>
+      <br />
+      {notes.map((note) => (
         <li key={note.id} onClick={() => toggleImportance(note)}>
-          {note.content} 
-          <strong> {note.important ? 'important' : ''}</strong>
+          {note.content}
+          <strong> {note.important ? "important" : ""}</strong>
         </li>
-      )}
+      ))}
     </div>
   )
 }
---
+++ src/request.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+
+const baseUrl = "http://localhost:3001/notes"
+
+export const getNotes = () => {
+  return axios.get(baseUrl)
+    .then(resp => resp.data)
+}
+
+export const createNote = newNote => axios.post(baseUrl, newNote).then(res => res.data)
+
+export const updateNote = (updatedNote) =>
+  axios.put(`${baseUrl}/${updatedNote.id}`, updatedNote).then((res) => res.data)
